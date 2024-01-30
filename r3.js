c2328c4745058f7a2c4ab07f08bd2f0d3db5764e
unknown <andre.sacilotto.dev@gmail.com>
2024-01-30 11:14:57 -0300

    division by 0

--- r3.js
+++ r3.js
@@ -9,14 +9,14 @@ const inputs = [
 inputs.forEach(x => {
     x.onchange = recalculate;
     x.onkeyup = recalculate;
-    x.onclick = (ev) =>{
-        if(!ev.shiftKey)
+    x.onclick = (ev) => {
+        if (!ev.shiftKey)
             return;
 
         const di = getDisabledInput();
         di.disabled = false;
         di.value = ev.target.value;
-        
+
         ev.target.disabled = true;
         ev.target.value = 0;
 
@@ -46,7 +46,7 @@ document.getElementById("CD").onclick = () => switchInput(2, 3);
 document.getElementById("AC").onclick = () => switchInput(0, 2);
 document.getElementById("BD").onclick = () => switchInput(1, 3);
 
-function getDisabledInput(){
+function getDisabledInput() {
     return inputs.find(x => x.disabled);
 }
 
@@ -83,37 +83,39 @@ function recalculate() {
     })
 
     // console.log(c);
-    if (c !== 0)
-        return;
+    if (c === 0) {
+        // a * d = b * c 
+        inputs.every((el, i) => {
+            if (!el.disabled)
+                return true;
 
-    // a * d = b * c 
-    inputs.every((el, i) =>{
-        if(!el.disabled)
-            return true;
+            // 1 0 0 1
+            // 2 3 3 2
+            // 0 1 2 3
+            // let [a,b] = index === 0 || index === 3 ? [1,2] : [0,3];
 
-        // 1 0 0 1
-        // 2 3 3 2
-        // 0 1 2 3
-        // let [a,b] = index === 0 || index === 3 ? [1,2] : [0,3];
+            const i0 = inputs[Math.floor(i * (i - 3) / 2 + 1)];
+            const i1 = inputs[Math.floor(i * (-i + 3) / 2 + 2)];
+            const i2 = inputs[inputs.length - i - 1];
+            const r = inputs[i];
+            
+            if(i2.value !== "0"){
+                const value = parseFloat(i0.value) * parseFloat(i1.value) / parseFloat(i2.value);
+                r.value = Number.isInteger(value) ? value : value.toFixed(dp.value);
+                pFormula.innerHTML = `${r.id} (${r.value}) = ${i0.id} (${i0.value}) * ${i1.id} (${i1.value}) / ${i2.id} (${i2.value})`;
+            }
+            else
+                errText += `The divisor <b>${i2.id}</b> is 0`;
 
-        const i0 = inputs[Math.floor(i * (i - 3) / 2 + 1)];
-        const i1 = inputs[Math.floor(i * (-i + 3) / 2 + 2)];
-        const i2 = inputs[inputs.length-i-1];
-        const r = inputs[i];
+            return false;
+        })
+    }
 
-        const value = parseFloat(i0.value) * parseFloat(i1.value) / parseFloat(i2.value);
-        r.value = Number.isInteger(value) ? value : value.toFixed(dp.value);
-
-        pFormula.innerHTML = `${r.id} (${r.value}) = ${i0.id} (${i0.value}) * ${i1.id} (${i1.value}) / ${i2.id} (${i2.value})`;
-
-        return false;
-    }) && console.error("Formula Error");
-
-    if(errText === ""){
+    if (errText === "") {
         pErr.style.display = "none";
         pErr.innerHTML = "";
     }
-    else{
+    else {
         pErr.style.display = "";
         pErr.innerHTML = errText;
     }
